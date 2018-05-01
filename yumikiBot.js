d2a8c78e173452e1753de1917a68bf59209053ad
Pierre Frost <Nuura@users.noreply.github.com>
2018-05-01 11:29:01 +0200

    Update yumikiBot.js

--- yumikiBot.js
+++ yumikiBot.js
@@ -26,12 +26,17 @@ bot.on('guildMemberAdd', member => {
 bot.on('message', message => {
     if(message.channel.name === "regles")
         {
-        if(message.content === "Ainsoph" && !message.member.roles.has('440658070918529024')) { //Check si l'user a le role Invoc {                                                                                                                                                    
-            message.member.addRole('440658070918529024'); //Mets le role normal                                                                                                                                                                                                       
-            message.member.removeRole('440617324492488705'); //Enleve le role invité                                                                                                                                                                                                  
-            message.delete();
+        if(message.content === "Ainsoph" && !message.member.roles.has('440658070918529024')) {
+          message.channel.send("Bien ! Yumiki a également besoin de ton pseudo en jeu ! :fox: Je t'écoute");
+         bot.on('message', message => {
+           message.member.setNickname(message.content);
+           message.member.addRole('440658070918529024'); //Mets le role normal                                                                                                                                                                                                       
+           message.member.removeRole('440617324492488705'); //Enleve le role invité
+           message.delete();
+         });
+          //Check si l'user a le role Invoc {                                                                                                                                                    
         }
-        else
+          else
             message.delete();
    }
 });
