ca432ecc74847e58a688eae18d4cbb1f3f537f3b
Pierre Frost <Nuura@users.noreply.github.com>
2018-05-01 09:37:48 +0200

    Update yumikiBot.js

--- yumikiBot.js
+++ yumikiBot.js
@@ -44,8 +44,7 @@ bot.on('message', message => {
        message.channel.send("Je suis un bot, qui sera amélioré au fur et a mesure du temps ! Pour le moment je m'occupe de la vérification !");
        message.channel.send("Afin d'entrer dans ce serveur, il y a quelques règles a respecter ! A savoir :");
        message.channel.send("-----------------------------");
-       message.channel.send("1 - Ce channel est réservé aux joueurs de la guilde Apachee, il est interdit d'ajouter d'autres membres (Sauf si une demande a été faites auprès de mon créateur ! :fox:), et de ces joueurs auront le rôle d'<@&440664569019629619>, avec un vocal et u
-n channel réservé !");
+       message.channel.send("1 - Ce channel est réservé aux joueurs de la guilde Apachee, il est interdit d'ajouter d'autres membres (Sauf si une demande a été faites auprès de mon créateur ! :fox:), et de ces joueurs auront le rôle d'<@&440664569019629619>, avec un vocal et un channel réservé !");
        message.channel.send("2 - De nouveaux rôles et channels vont voir le jour prochainement, concernant d'autres jeux ! N'hésitez pas a proposer vos idées à mon créateur ! :wolf:");
        message.channel.send("3 - Pour tout ce qui est discussion farfelue, il faudra s'adresser au channel <#440665131731648516>, les autres sont réservés pour des choses plus sérieuses ! :ok_hand:");
        message.channel.send("4 - Ce serveur est conçu pour partager des informations, échanger et s'entraider ! Les personnes qui mettrait le désorde dans le serveur de mon création sera expulsé, ou pire banni :scream_cat:");
